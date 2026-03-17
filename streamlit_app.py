efa85b341eb2107eddb4e5265c3dafd8ad070b28
Henrique Waideman Peres <149389393+hwperes@users.noreply.github.com>
2026-03-16 23:21:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,8 +139,8 @@ def load_models_and_config():
 
     """Carrega o modelo treinado e o arquivo de configuração usando caminhos relativos."""
     
-    caminho_modelo = os.path.join("modelos", "modelo_passosmagicos.pkl")
-    caminho_config = os.path.join("modelos", "configuracao_passosmagicos.pkl")
+    caminho_modelo = os.path.join("models", "modelo_passos_magicos.pkl")
+    caminho_config = os.path.join("models", "config_passos_magicos.pkl")
     
     try:
         modelo = joblib.load(caminho_modelo)
